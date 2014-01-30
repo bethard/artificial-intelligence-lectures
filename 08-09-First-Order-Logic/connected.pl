6a4c029c6da7612cd0d2020a7dad77f5404ac3b2
Steven Bethard <steven.bethard@gmail.com>
2014-01-30 00:09:18 -0600

    Revises backwards chaining backtracking example to use forest package

--- 08-09-First-Order-Logic/connected.pl
+++ 08-09-First-Order-Logic/connected.pl
@@ -1,9 +1,8 @@
 edge(a, b).
 edge(b, c).
 edge(b, d).
-edge(c, e).
 edge(d, f).
 connected(X, Z):- edge(X, Z).
-connected(X, Z):- connected(Y, Z), edge(X, Y).
+connected(X, Z):- edge(X, Y), connected(Y, Z).
 
 

b7a07a85c8a1ff5b838f2346617f0724f177fdf1
Steven Bethard <steven.bethard@gmail.com>
2014-02-18 21:36:32 -0600

    Updates sampling code to use {} as assignment instead of [], and to use better names throughout

--- 14-Probabilistic-Reasoning/prob.py
+++ 14-Probabilistic-Reasoning/prob.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import random
 
-class Node(object):
+class Variable(object):
 
     def __init__(self, *parents):
         self.parents = parents
@@ -10,10 +10,10 @@ class Node(object):
         for parent in self.parents:
             parent.children.append(self)
 
-    def add_prob(self, prob, *parent_values):
+    def add_probability(self, prob, *parent_values):
         self.prob_table[parent_values] = prob
 
-    def get_prob(self, value, *parent_values):
+    def probability_of(self, value, *parent_values):
         prob = self.prob_table[parent_values]
         if value is True:
             return prob
@@ -22,56 +22,54 @@ class Node(object):
 
 class BayesNet(object):
 
-    def __init__(self, nodes):
-        self.nodes = nodes
-        for i, node in enumerate(nodes):
-            node.index = i
+    def __init__(self, variables):
+        self.variables = variables
 
     def __iter__(self):
-        return iter(self.nodes)
+        return iter(self.variables)
 
     def __len__(self):
-        return len(self.nodes)
+        return len(self.variables)
 
 
-B = Node()
-B.add_prob(0.001)
-E = Node()
-E.add_prob(0.002)
+B = Variable()
+B.add_probability(0.001)
+E = Variable()
+E.add_probability(0.002)
 
-A = Node(B, E)
-A.add_prob(0.95, True, True)
-A.add_prob(0.94, True, False)
-A.add_prob(0.29, False, True)
-A.add_prob(0.001, False, False)
+A = Variable(B, E)
+A.add_probability(0.95, True, True)
+A.add_probability(0.94, True, False)
+A.add_probability(0.29, False, True)
+A.add_probability(0.001, False, False)
 
-J = Node(A)
-J.add_prob(0.90, True)
-J.add_prob(0.05, False)
+J = Variable(A)
+J.add_probability(0.90, True)
+J.add_probability(0.05, False)
 
-M = Node(A)
-M.add_prob(0.70, True)
-M.add_prob(0.01, False)
+M = Variable(A)
+M.add_probability(0.70, True)
+M.add_probability(0.01, False)
 
 alarm_net = BayesNet([B, E, A, J, M])
 alarm_args = B, {J: True, M: True}, alarm_net, 10000
 
-C = Node()
-C.add_prob(0.5)
+C = Variable()
+C.add_probability(0.5)
 
-S = Node(C)
-S.add_prob(0.10, True)
-S.add_prob(0.50, False)
+S = Variable(C)
+S.add_probability(0.10, True)
+S.add_probability(0.50, False)
 
-R = Node(C)
-R.add_prob(0.80, True)
-R.add_prob(0.20, False)
+R = Variable(C)
+R.add_probability(0.80, True)
+R.add_probability(0.20, False)
 
-WG = Node(S, R)
-WG.add_prob(0.99, True, True)
-WG.add_prob(0.90, True, False)
-WG.add_prob(0.90, False, True)
-WG.add_prob(0.00, False, False)
+WG = Variable(S, R)
+WG.add_probability(0.99, True, True)
+WG.add_probability(0.90, True, False)
+WG.add_probability(0.90, False, True)
+WG.add_probability(0.00, False, False)
 
 wet_net = BayesNet([C, S, R, WG])
 wet_args = R, {S: True}, wet_net, 10000
@@ -80,26 +78,26 @@ wet_args = R, {S: True}, wet_net, 10000
 def prior_sample(bayes_net):
     # generate a value for each variable in the network
     # variables are sorted from parents to children
-    sample = []
-    for node in bayes_net:
+    sample = {}
+    for variable in bayes_net:
         # find the values assigned to the parents
-        parent_samples = [sample[p.index] for p in node.parents]
+        parent_values = [sample[parent] for parent in variable.parents]
         # find the probability for this assignment from the table
-        prob = node.get_prob(True, *parent_samples)
+        probability = variable.probability_of(True, *parent_values)
         # add True or False according to the distribution
-        sample.append(random.random() < prob)
+        sample[variable] = random.random() < probability
     # return the complete sample
     return sample
 
 
-def rejection_sampling(query_var, evidence, bayes_net, samples):
+def rejection_sampling(query, evidence, bayes_net, samples):
     # generate a bunch of samples, counting query values
     counts = {False: 0, True: 0}
     for _ in range(samples):
         sample = prior_sample(bayes_net)
         # if the sample is consistent with the evidence, count it
-        if all(sample[v.index] == evidence[v] for v in evidence):
-            counts[sample[query_var.index]] += 1
+        if all(sample[variable] == evidence[variable] for variable in evidence):
+            counts[sample[query]] += 1
     # normalize the counts and return the probabilities
     return normalize(counts)
 
@@ -109,7 +107,6 @@ def normalize(counts):
     total = sum(counts.values())
     for value in counts:
         counts[value] /= total
-    # return the probabilities
     return counts
 
 print 'Rejection Sampling'
@@ -119,31 +116,32 @@ print 'P(B|j,m)', rejection_sampling(*alarm_args)
 print 'P(R|s)', rejection_sampling(*wet_args)
 
 
-def weighted_sample(bayes_net, evidence):
-    # generate a value for each variable from roots to leaves
-    sample, weight = [], 1.0
-    for node in bayes_net:
-        p_samples = [sample[p.index] for p in node.parents]
-        # if the value is given, add it and update the weight
-        if node in evidence:
-            weight *= node.get_prob(evidence[node], *p_samples)
-            sample.append(evidence[node])
-        # otherwise, add True or False using the distribution
-        else:
-            prob = node.get_prob(True, *p_samples)
-            sample.append(random.random() < prob)
-    # return the weighted sample
-    return sample, weight
-
-
-def likelihood_weighting(query_var, evidence, bayes_net, samples):
+def likelihood_weighting(query, evidence, bayes_net, samples):
     # generate samples, adding up weights for each query value
     counts = {False: 0, True: 0}
     for _ in range(samples):
         sample, weight = weighted_sample(bayes_net, evidence)
-        counts[sample[query_var.index]] += weight
+        counts[sample[query]] += weight
     # normalize the counts and return the probabilities
     return normalize(counts)
+
+
+def weighted_sample(bayes_net, evidence):
+    # generate a value for each variable, from parents to children
+    sample, weight = {}, 1.0
+    for variable in bayes_net:
+        parent_values = [sample[parent] for parent in variable.parents]
+        # if the value is given, add it and update the weight
+        if variable in evidence:
+            sample[variable] = value = evidence[variable]
+            weight *= variable.probability_of(value, *parent_values)
+        # otherwise, add True or False using the distribution
+        else:
+            probability = variable.probability_of(True, *parent_values)
+            sample[variable] = random.random() < probability
+    # return the weighted sample
+    return sample, weight
+
 
 print 'Likelihood Weighting'
 # actual answer: {False:0.716, True: 0.284}
@@ -152,45 +150,45 @@ print 'P(B|j,m)', likelihood_weighting(*alarm_args)
 print 'P(R|s)', likelihood_weighting(*wet_args)
 
 
-def gibbs_sampling(query_var, evidence, bayes_net, samples):
+def gibbs_sampling(query, evidence, bayes_net, samples):
     # initialize the sample with random values for non-evidence
-    sample = []
-    for node in bayes_net:
-        if node in evidence:
-            sample.append(evidence[node])
+    sample = {}
+    for variable in bayes_net:
+        if variable in evidence:
+            sample[variable] = evidence[variable]
         else:
-            sample.append(random.random() < 0.5)
+            sample[variable] = random.random() < 0.5
     # generate samples by changing non-evidence values
     counts = {False:0, True:0}
-    non_evidence = [n for n in bayes_net if n not in evidence]
-    for i in range(samples):
-        for node in non_evidence:
+    non_evidence = [var for var in bayes_net if var not in evidence]
+    for _ in range(samples):
+        for variable in non_evidence:
             # get the prob distribution given the markov blanket
-            probs = get_probs_given_markov_blanket(node, sample)
+            probability = markov_blanket_probability_of(variable, sample)
             # select a new value according to that distribution
-            sample[node.index] = random.random() < probs[True]
+            sample[variable] = random.random() < probability
             # increment the count for the current query value
-            counts[sample[query_var.index]] += 1
+            counts[sample[query]] += 1
     # normalize the counts and return the probabilities
     return normalize(counts)
 
 
-def get_probs_given_markov_blanket(node, sample):
+def markov_blanket_probability_of(variable, sample):
     # get the probabilities for each value of the variable
     counts = {}
     for value in [True, False]:
-        # change the node's value in the sample
-        sample[node.index] = value
-        # the probability of the node given its parents
-        p_samples = [sample[p.index] for p in node.parents]
-        counts[value] = node.get_prob(value, *p_samples)
-        # times probabilities of children given their parents
-        for child in node.children:
-            c_sample = sample[child.index]
-            p_samples = [sample[p.index] for p in child.parents]
-            counts[value] *= child.get_prob(c_sample, *p_samples)
-    # normalize the counts and return the probabilities
-    return normalize(counts)
+        # change the variable's value in the sample
+        sample[variable] = value
+        # add the probability of the variable given its parents
+        parent_values = [sample[parent] for parent in variable.parents]
+        counts[value] = variable.probability_of(value, *parent_values)
+        # times the probabilities of the children given their parents
+        for child in variable.children:
+            child_value = sample[child]
+            parent_values = [sample[parent] for parent in child.parents]
+            counts[value] *= child.probability_of(child_value, *parent_values)
+    # normalize the counts and return the probability of True
+    return normalize(counts)[True]
 
 
 #print gibbs_sampling(R, {S: True}, wet_net, 10)

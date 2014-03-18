0834c0c782bb150734e6ee05e1d8779ebc87c136
Steven Bethard <steven.bethard@gmail.com>
2014-03-18 15:42:13 -0500

    Adds ordering to factor printing for easier reading

--- 14-Probabilistic-Reasoning/factor.scala
+++ 14-Probabilistic-Reasoning/factor.scala
@@ -1,3 +1,5 @@
+import scala.math.Ordering.Implicits._
+
 // This is far from efficient. But at least it verifies the calculations in the book.
 
 class Variable[VAL](val symbol: Symbol, val values: List[VAL]) {
@@ -19,7 +21,7 @@ object Variable {
   }
 }
 
-case class Factor[VAL](
+case class Factor[VAL: Ordering](
   variables: List[Variable[VAL]],
   assignmentScores: Map[Map[Variable[VAL], VAL], Double]) {
 
@@ -51,11 +53,16 @@ case class Factor[VAL](
   }
 
   override def toString = {
-    this.assignmentScores.mkString("", "\n", "\n")
+    implicit val ordering = Ordering.by{
+      (assignment: Map[Variable[VAL],VAL]) => assignment.toSeq.map{
+        case (variable, value) => (variable.symbol.toString, value)
+      }
+    }
+    this.assignmentScores.toSeq.sorted.mkString("", "\n", "\n")
   }
 }
 object Factor {
-  def apply[VAL](
+  def apply[VAL : Ordering](
     variables: List[Variable[VAL]],
     assignments: (List[VAL], Double)*): Factor[VAL] = {
     Factor(variables, assignments.map{
@@ -66,6 +73,7 @@ object Factor {
 
 val A = new Variable('A, List(true, false))
 val B = new Variable('B, List(true, false))
+val C = new Variable('B, List(true, false))
 val E = new Variable('E, List(true, false))
 
 val X = new Variable('X, List("a", "b", "c"))
@@ -111,3 +119,19 @@ println(pE * (pABE * pjA * pmA - A) - E)
 println(pB * (pE * (pABE * pjA * pmA - A) - E))
 println((pB * pE * pABE * pjA * pmA - A - E).normalized)
 
+val ab = Factor(
+  List(A, B),
+  List(true, true) -> 0.3,
+  List(true, false) -> 0.1,
+  List(false, true) -> 0.7,
+  List(false, false) -> 0.9)
+
+val bc = Factor(
+  List(B, C),
+  List(true, true) -> 0.6,
+  List(true, false) -> 0.8,
+  List(false, true) -> 0.4,
+  List(false, false) -> 0.2)
+
+println(ab * bc)
+println((ab * bc) - B)
